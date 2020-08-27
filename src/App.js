0331ac549a061099a01301895ccd17cd461300d7
austinwilson1224 <austin1224@gmail.com>
2020-08-27 15:07:42 -0700

    flip move and edit items

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
   }
 
   handleInput(e) {
@@ -53,6 +54,18 @@ class App extends React.Component {
     })
   }
 
+  setUpdate(text, key) {
+    const items = this.state.items;
+    items.map(item => {
+      if(item.key===key) {
+        item.text=text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+  }
+
 
   render() {
     return(
@@ -66,7 +79,9 @@ class App extends React.Component {
           </form>
         </header>
         <ListItems items = {this.state.items}
-        deleteItem = {this.deleteItem}></ListItems>
+        deleteItem = {this.deleteItem}
+        setUpdate = {this.setUpdate}
+        ></ListItems>
       </div>
       
     )
--- src/ListItems.js
+++ src/ListItems.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './ListItems.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import FlipMove from 'react-flip-move'
 
 function ListItems(props) {
     const items = props.items;
     const listItems = items.map(item => {
         return <div className="list" key={ item.key }>
             <p>
-                {item.text}
+                <input type="text" 
+                 id={item.key} 
+                 value={item.text}
+                 onChange= {
+                    (e) => props.setUpdate(e.target.value, item.key)
+                 }/>
                 <span>
                 <FontAwesomeIcon className="faicons" 
                 icon="trash"
@@ -18,7 +24,11 @@ function ListItems(props) {
         </div>
     })
     return(
-        <div>{listItems}</div>
+        <div>
+            <FlipMove duration = {300} easing="ease-in-out">
+                {listItems}
+            </FlipMove>
+        </div>
     )
 }
 
